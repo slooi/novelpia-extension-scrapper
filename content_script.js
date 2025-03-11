0e20aa1db908e0847c720a6973b7758c8e8a3782
s <s.looi2@outlook.com>
2025-03-11 19:59:23 +0900

    :sparkles: make basic chapter number and text scraper with prepended prompt

---
+++ content_script.js
@@ -0,0 +1,26 @@
+// content_script.js
+let dataToSend = 1; // Store the data to be sent
+
+// Function to set the data to be sent when the popup opens
+function setDataToSend(data) {
+	dataToSend = data;
+}
+
+// Listen for the message from popup.js
+browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+	if (message.greeting === "Popup opened!") {
+		sendResponse({
+			title: getTextContent(".menu-title-wrapper"),
+			text: getTextContent("#novel_drawing"),
+		});
+	}
+});
+
+function getTextContent(selector) {
+	const elements = document.querySelectorAll(selector);
+	if (elements && elements.length > 0) {
+		const element = elements[0];
+		return element.textContent;
+	}
+	return "N/A";
+}
---
+++ popup.js
@@ -0,0 +1,34 @@
+// popup.js
+const PROMPT = `You are an expert Korean translator with more than 50 years of experience translating from Korean to English. Translate the below. If you translate it well, i will tip you 1000 USD. This is a fan fic of Overlord and Frieren. It features Momonga(male, undead) in Frieren's world, it also features Serie(female elf, sensei like character that teaches Flamme and Momomga/Ainz (though Ainz insists on being in a cooperative relationship)) and Flamme(female human). Serie picked Ainz up at sea after they engaged in a life and death fight before settling things and forming their cooperative relationship.`;
+
+const outputDiv = document.getElementById("output");
+
+function displayData(data) {
+	document.body.innerText = data;
+}
+
+// Send message to get data when the popup opens
+window.onload = function () {
+	browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
+		browser.tabs
+			.sendMessage(tabs[0].id, { greeting: "Popup opened!" })
+			.then((response) => {
+				if (response) {
+					console.log("response", response);
+					displayData(`
+                        ${PROMPT}
+
+                        ${response.title}                        
+
+                        ${response.text
+							.split("\n")
+							.map((line) => line.trim())
+							.join("\n")}
+                        `);
+				}
+			})
+			.catch((error) => {
+				console.error("Error sending message:", error);
+			});
+	});
+};
